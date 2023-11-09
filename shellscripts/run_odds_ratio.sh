57b3e43161e5e88ed2b9f8ad61d12a587005a560
pravich2 <pravich2@login01.cm.cluster>
2023-11-09 15:03:07 -0500

    Adding plotting notebook to evaluate data aggregation

---
+++ shellscripts/run_odds_ratio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH
+#SBATCH --job-name=run_odds
+#SBATCH --time=1:0:0
+#SBATCH --partition=defq
+#SBATCH --mem=40G
+
+
+module load r 
+homeDir=`pwd | sed -e 's/\/shellscripts//g'`
+echo $homeDir
+scriptDir=$homeDir/src/04_evaluate_data_aggregation
+logDir=$homeDir/logs
+
+
+Rscript $scriptDir/20-compute_odds_ratio.R $1 $2 $3 
